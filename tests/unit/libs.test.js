caf83ebdaa8c1782aa2e31dc9f0632bb0f6a520c
jirawat070 <jirawat_pao@hotmail.co.th>
2018-11-16 00:35:39 +0700

    testing number

--- tests/unit/libs.test.js
+++ tests/unit/libs.test.js
@@ -1,7 +1,16 @@
-test ('My first unit test',() =>{
-    // arrange
-    
-    // action
+const libs = require('../../src/libs');
 
+test ('absolute -should return a positive number if input is positive',() =>{
+    // arrange
+    const input = 1;
+    // action
+    const result = libs.absolute(input);
     // Asset
+    expect(result).toBe(input);
+});
+
+test ('absolute -should return a positive number if input is negative',() =>{
+});
+
+test ('absolute -should return 0 if input is 0',() =>{
 });
